2b596b62f03962596eae4321660e3d8923679376
Felix A. Schultz <felix.schultz@engapho.com>
2025-03-23 15:28:01 +0100

    feat: Update Dashboard component header to reflect 'User Consent Interaction'

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -97,7 +97,7 @@ export default function Dashboard(props) {
                     <Crawler />
                 </div> */}
                 <div className="" style={{ paddingTop: "40px" }}>
-                    <h2>User Interactions</h2>
+                    <h2>User Consent Interaction</h2>
                     <div className="grid-container grid-2" style={{ gridTemplateColumns: "1fr .5fr", gap: "20px" }}>
                         {(loading) ? <>
 
