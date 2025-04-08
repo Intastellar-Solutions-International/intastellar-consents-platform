8a4cd43ecc0d8c78760dd63e0e1f7bda67814f0b
Felix A. Schultz <felix.schultz@engapho.com>
2025-04-08 21:41:37 +0200

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,10 @@
 import "./Style.css";
+import logo from "../Header/logo.png";
 export default function Footer() {
     const year = new Date().getFullYear();
     return <>
         <footer className="footer dashboard-content">
+            <img src={logo} alt="Intastellar Solutions International" className="footer-logo" />
             <p>&copy; {year} Intastellar Solutions International. All rights reserved.</p>
             <p><a href="https://www.intastellarsolutions.com/about/legal/terms" target="_blank" className="links">Terms of Service</a></p>
         </footer>
