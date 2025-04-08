8d86ce45ab1731cbd0cf1ba4f8e7d6a271db721f
Felix A. Schultz <felix.schultz@engapho.com>
2025-04-08 21:37:08 +0200

    Fixed header styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import "./Style.css";
-export default function Footer(){
+export default function Footer() {
     const year = new Date().getFullYear();
     return <>
         <footer className="footer dashboard-content">
-            <p>&copy; {year} Intastellar Solutions International. | <a href="https://www.intastellarsolutions.com/about/legal/terms" target="_blank" className="links">Terms of Service</a></p>
+            <p>&copy; {year} Intastellar Solutions International. All rights reserved.</p>
+            <p><a href="https://www.intastellarsolutions.com/about/legal/terms" target="_blank" className="links">Terms of Service</a></p>
         </footer>
     </>
 }
--- src/components/header/Sticky/index.js
+++ src/components/header/Sticky/index.js
@@ -1,20 +1,20 @@
 import Filter from "../../Filter/index.js";
-export default function StickyPageTitle({title, url, method, header, setLastDays, getLastDays, setActiveData, fromDate, toDate, setFromDate, setToDate}){
+export default function StickyPageTitle({ title, url, method, header, setLastDays, getLastDays, setActiveData, fromDate, toDate, setFromDate, setToDate }) {
     window.addEventListener("scroll", (e) => {
-        if(window.scrollY > 0){
+        if (window.scrollY > 0) {
             document.querySelector(".infoHeader").classList.add("sticky");
-        }else{
+        } else {
             document.querySelector(".infoHeader").classList.remove("sticky");
         }
     })
     return <>
-        <div className="infoHeader" style={{padding: "40px 0"}}>
+        <div className="infoHeader" style={{ padding: "10px 0" }}>
             <div className="dashboard-content" style={{
                 display: "grid",
                 gridTemplateColumns: "1fr .5fr",
                 alignItems: "center",
             }}>
-                <h1 style={{fontSize: "1.5em"}}>{title}</h1>
+                <h1 style={{ fontSize: "1.5em" }}>{title}</h1>
                 {(url) ? <Filter url={url} method={method} header={header} setLastDays={setLastDays} getLastDays={getLastDays} setActiveData={setActiveData} fromDate={fromDate} toDate={toDate} setFromDate={setFromDate} setToDate={setToDate} /> : null}
             </div>
         </div>
