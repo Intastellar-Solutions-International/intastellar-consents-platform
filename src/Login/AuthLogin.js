e6cd5a38986c3a611e91642d9454a683440a3ff2
Felix A. Schultz <fs@asasoftware.aero>
2025-04-10 09:11:30 +0200

    Remove login form rendering from AuthLogin component

--- src/Login/AuthLogin.js
+++ src/Login/AuthLogin.js
@@ -48,10 +48,5 @@ export default function AuthLogin() {
     // You can now use the token in your application logic
     // For example, you might want to store it in local storage or use it to authenticate a user
 
-    return <div className="loginForm-container">
-        <form className="loginForm" onSubmit={(e) => { e.preventDefault() }}>
-            <h1 className="loginForm-title">Sign in to Intastellar Consents</h1>
-            <label>Login successful, you can close this window</label>
-        </form>
-    </div>;
+    return null;
 }
