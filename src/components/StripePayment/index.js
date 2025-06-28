a2f21425592fda59971846ad88f2e3c546ea9b6f
Felix A. Schultz <felix.schultz@engapho.com>
2025-06-28 12:21:44 +0200

    fix: Adjust formatting in StripePayment component for improved readability

--- src/components/StripePayment/index.js
+++ src/components/StripePayment/index.js
@@ -21,12 +21,13 @@ export default function StripePayment(props) {
                 <Select items={allOrganisations} onChange={(e) => {
                     localStorage.setItem("organisation", e);
                     window.location.reload();
-                }} defaultValue={companyName}/>
+                }} defaultValue={companyName} />
                 <h1>Choose a Plan</h1>
                 <p>Choose a plan that suits your needs. You´re about to select a plan for your company: {companyName}</p>
                 <stripe-pricing-table class="stripe-price-table" pricing-table-id="prctbl_1OGmIdEK0yX4gMoH7rRqdg9y"
-                publishable-key="pk_test_cdjFXrTVnj1SdyYXzlTz95Sk" customer-email={props.userId()}
-                client-reference-id={Authentication.getOrganisation()}>
+                    publishable-key="pk_test_cdjFXrTVnj1SdyYXzlTz95Sk" customer-email={props.userId()}
+                    client-reference-id={Authentication.getOrganisation()}
+                >
                 </stripe-pricing-table>
             </div>
             <footer className="footer">
