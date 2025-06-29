4c717de0a57ccf42837820f56e05da64b53f823e
Felix A. Schultz <felix.schultz@engapho.com>
2025-06-29 10:55:00 +0200

    Update Stripe pricing table ID in StripePayment component

--- src/components/StripePayment/index.js
+++ src/components/StripePayment/index.js
@@ -24,7 +24,7 @@ export default function StripePayment(props) {
                 }} defaultValue={companyName} />
                 <h1>Choose a Plan</h1>
                 <p>Choose a plan that suits your needs. You´re about to select a plan for your company: {companyName}</p>
-                <stripe-pricing-table class="stripe-price-table" pricing-table-id="prctbl_1OGmIdEK0yX4gMoH7rRqdg9y"
+                <stripe-pricing-table class="stripe-price-table" pricing-table-id="prctbl_1RfGbIEK0yX4gMoHvanLTmrU"
                     publishable-key="pk_test_cdjFXrTVnj1SdyYXzlTz95Sk" customer-email={props.userId()}
                     client-reference-id={Authentication.getOrganisation()}
                 >
