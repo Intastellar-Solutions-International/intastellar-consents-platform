feb40cfbd072b58976f128b540b4627f6e12394d
Felix A. Schultz <felix.schultz@intastellarsolutions.com>
2026-01-02 20:10:06 +0100

    Refactor PremiumTier layout and enhance Widget component styling
    - Changed the grid layout in PremiumTier from 5 columns to 3 for better visual organization.
    - Updated the Widget component to allow custom class names for more flexible styling.
    - Cleaned up the JSX structure in the Widget component for improved readability.

--- src/components/widget/widget.js
+++ src/components/widget/widget.js
@@ -2,16 +2,15 @@ import "./Widget.css";
 import Line from "../Charts/Line";
 export default function Widget(props) {
     const overViewTotal = (props?.overviewTotal) ? " overviewTotal" : " overviewDistribution";
+    const className = (props?.class) ? props.class : "";
 
     return (
-        <>
-            <div className={"widget" + overViewTotal}>
-                <h2 className="overvieTotal-num">{(props?.totalNumber?.Total) ? props?.totalNumber?.Total?.toLocaleString("de-DE") : props?.totalNumber}</h2>
-                <p>{props?.type}</p>
-                {
-                    (props?.totalNumber?.dailyNum) ? <Line title="Consents giving" data={props?.totalNumber?.dailyNum} data2={props?.totalNumber?.dailyNum.previousPeriod} fromDate={props?.fromDate} toDate={props?.toDate} /> : null
-                }
-            </div>
-        </>
+        <div className={className + " widget" + overViewTotal}>
+            <h2 className="overvieTotal-num">{(props?.totalNumber?.Total) ? props?.totalNumber?.Total?.toLocaleString("de-DE") : props?.totalNumber}</h2>
+            <p>{props?.type}</p>
+            {
+                (props?.totalNumber?.dailyNum) ? <Line title="Consents giving" data={props?.totalNumber?.dailyNum} data2={props?.totalNumber?.dailyNum.previousPeriod} fromDate={props?.fromDate} toDate={props?.toDate} /> : null
+            }
+        </div>
     )
 }
