47299e1669d138c575ae7a4704fe6ec530c3f91a
Felix A. Schultz <felix.schultz@intastellarsolutions.com>
2026-01-02 19:33:11 +0100

    Update LiveView title to reflect interactions data for the last 30 minutes

--- src/components/LiveView/index.js
+++ src/components/LiveView/index.js
@@ -17,7 +17,7 @@ export function LiveView(props) {
             (!loading) ?
                 <div className="liveView">
                     <div className="liveView-content">
-                        <p className="liveView-content-title">USERS IN LAST 30 MINUTES</p>
+                        <p className="liveView-content-title">INTERACTIONS IN LAST 30 MINUTES</p>
                         <div className="liveView-content-data">
                             <div className="liveView-content-data-1">
                                 <p className="liveView-content-data-1-number">{liveData?.count}</p>
